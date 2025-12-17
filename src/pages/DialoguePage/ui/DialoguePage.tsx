a1eff2b80d08ca2a46afdb197e059f3f7f25e9e0
lizka <lizka4231@gmail.com>
2025-12-17 23:08:54 +0300

    feat(dialogue ui): add click handling

--- src/pages/DialoguePage/ui/DialoguePage.tsx
+++ src/pages/DialoguePage/ui/DialoguePage.tsx
@@ -1,4 +1,5 @@
 import { type FC } from 'react';
+import { useNavigate } from 'react-router-dom';
 
 import { DialogueItem } from '@entities/dialogue/ui/DialogueItem';
 import { useAppSelector } from '@shared/store/hooks';
@@ -6,7 +7,8 @@ import { useAppSelector } from '@shared/store/hooks';
 import styles from './DialoguePage.module.scss';
 
 export const DialoguePage: FC = () => {
-   const { messages } = useAppSelector((state) => state.messages)
+   const { messages } = useAppSelector((state) => state.messages);
+   const navigate = useNavigate();
 
    if (!messages.length) {
       return <p>У вас пока нет диалогов</p>
@@ -20,7 +22,7 @@ export const DialoguePage: FC = () => {
                dialogueId={dialogue.id}
                dialogueWith={dialogue.dialogueWith}
                iconUrl={dialogue.dialogueWithIconUrl}
-               onClick={() => {}}
+               onClick={() => navigate(`/dialogue/${dialogue.id}`)}
             />
          )}
       </div>
