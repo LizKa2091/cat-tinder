d7d5f1ed059e0d350a1007f957739f5e09c41f0c
Liza <lizka4231@gmail.com>
2025-12-19 14:06:29 +0300

    feat(cat ui): add match item

---
+++ src/entities/cat/ui/match-item/MatchItem.tsx
@@ -0,0 +1,28 @@
+import { type FC } from 'react';
+
+import { ActionButton } from '@shared/ui/action-button';
+import { useStartDialogue } from '@entities/dialogue/model/useStartDialogue';
+import type { ICatItem } from '@entities/cat/types';
+
+import styles from './MatchItem.module.scss';
+
+interface IMatchItemProps {
+   catImage: string;
+   catData: ICatItem;
+}
+
+export const MatchItem: FC<IMatchItemProps> = ({ catImage, catData }) => {
+   const { startDialogue } = useStartDialogue();
+   const { title, first, last } = catData.catData.name;
+   const fullCatName: string = title + ' ' + first + ' ' + last;
+
+   return (
+      <div className={styles.container}>
+         <img src={catImage} alt={`${fullCatName} image`} className={styles.img} />
+         <div className={styles.catName}>
+            <p>{fullCatName}</p>
+         </div>
+         <ActionButton type='message' onClick={() => startDialogue(catData)} />
+      </div>
+   )
+}
